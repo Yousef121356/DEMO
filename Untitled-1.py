a98c32a5182498942ba997481df4db2e47608888
Yousef121356 <168172910+Yousef121356@users.noreply.github.com>
2024-04-26 10:55:49 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+echo "# DEMO" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Yousef121356/DEMO.git
+git push -u origin main
